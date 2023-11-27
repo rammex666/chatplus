46235c2232eabbee6b145ffb109e835a37eea8fa
rammex <romain.kaspar@gmail.com>
2023-11-27 11:46:56 +0100

    scoreboard, chatformat panel

--- src/main/java/org/rammex/chatplus/ui/adminpanel.java
+++ src/main/java/org/rammex/chatplus/ui/adminpanel.java
@@ -15,8 +15,34 @@ public class adminpanel {
         Inventory inv = Bukkit.createInventory(null, 27, "Admin Panel");
 
         inv.setItem(11, getItem(Material.PAPER, ChatColor.RED+"Chat Format"));
-        inv.setItem(13, getItem(Material.BEDROCK, ChatColor.RED+"SOON"));
-        inv.setItem(15, getItem(Material.BEDROCK, ChatColor.RED+"SOON"));
+        inv.setItem(13, getItem(Material.BOOK, ChatColor.RED+"ScoreBoard"));
+        inv.setItem(15, getItem(Material.REDSTONE_WIRE, ChatColor.RED+"OverAll"));
+
+        player.openInventory(inv);
+    }
+
+    public static void chatformatpanel(Player player){
+        Inventory inv = Bukkit.createInventory(null, 27, "Chat Format");
+
+        inv.setItem(11, getItem(Material.ANVIL, ChatColor.AQUA+"Templates"));
+        inv.setItem(13, getItem(Material.PAPER, ChatColor.AQUA+"Actual"));
+        inv.setItem(15, getItem(Material.CRAFTING_TABLE, ChatColor.AQUA+"Change"));
+
+        inv.setItem(18, getItem(Material.BARRIER, ChatColor.RED+"Back"));
+
+
+        player.openInventory(inv);
+    }
+
+    public static void scoreboardpanel(Player player){
+        Inventory inv = Bukkit.createInventory(null, 27, "ScoreBoard");
+
+        inv.setItem(11, getItem(Material.ANVIL, ChatColor.AQUA+"Templates"));
+        inv.setItem(13, getItem(Material.PAPER, ChatColor.AQUA+"Actual"));
+        inv.setItem(15, getItem(Material.CRAFTING_TABLE, ChatColor.AQUA+"Change"));
+
+        inv.setItem(18, getItem(Material.BARRIER, ChatColor.RED+"Back"));
+
 
         player.openInventory(inv);
     }
