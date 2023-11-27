95ae3ea4030a326847133941ba09f546038d89d0
rammex <romain.kaspar@gmail.com>
2023-11-27 11:56:51 +0100

    event uiclicked

--- src/main/java/org/rammex/chatplus/events/UiClick.java
+++ src/main/java/org/rammex/chatplus/events/UiClick.java
@@ -1,12 +1,15 @@
 package org.rammex.chatplus.events;
 
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 import org.rammex.chatplus.Chatplus;
+import org.rammex.chatplus.ui.adminpanel;
 
 public class UiClick implements Listener {
     Chatplus plugin;
@@ -19,9 +22,18 @@ public class UiClick implements Listener {
         final Inventory inventory = e.getInventory();
         final Player whoClicked = (Player) e.getWhoClicked();
         final String title = e.getView().getTitle();
+        ItemStack current = e.getCurrentItem();
         if (title.equalsIgnoreCase("Admin Panel")){
             e.setCancelled(true);
             e.setResult(Event.Result.DENY);
+            if(current.getType() == Material.PAPER){
+                whoClicked.closeInventory();
+                adminpanel.chatformatpanel(whoClicked);
+            }
+            if(current.getType() == Material.BOOK){
+                whoClicked.closeInventory();
+                adminpanel.scoreboardpanel(whoClicked);
+            }
         };
     }
 }
